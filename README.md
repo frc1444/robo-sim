73a91473dc914ab99b4009dd5b362795159bb54e
Joshua Shannon <retrodaredevil@gmail.com>
2019-09-16 13:34:33 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# RoboSim
+A 2D simulation and abstraction of WPILib code for FRC 
+### Using
+This is in the very early stages of development and will not be ready for use for a while.
+
+### About
+This is a 2D simulation built using libGDX. To use this it is recommended to
+not use the WPILib API for anything as this project will provide as much abstraction as possible to avoid that.
---
+++ android/src/com/first1444/frc/sim/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.first1444.frc.sim;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.first1444.frc.sim.SimMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SimMain(), config);
+	}
+}
