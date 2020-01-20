b7deee8225c694c7b189a7639b26f8a16d47deb7
Joshua Shannon <retrodaredevil@gmail.com>
2020-01-20 14:53:00 -0600

    Removed debug prints

--- api/src/main/java/com/first1444/sim/api/drivetrain/swerve/AnyWheelSwerveDrive.kt
+++ api/src/main/java/com/first1444/sim/api/drivetrain/swerve/AnyWheelSwerveDrive.kt
@@ -32,11 +32,10 @@ class AnyWheelSwerveDrive(
             val map = HashMap<PositionSwerveModule, Double>(drivetrainData.positionSwerveModules.size)
             var max = 0.0
             for(positionModule in drivetrainData.positionSwerveModules){
-                val position = positionModule.position.normalized
-                println(position)
+                val normalPosition = positionModule.position.normalized
                 val module = positionModule.swerveModule
-                val wheelX = x + turnAmount * position.y
-                val wheelY = y - turnAmount * position.x
+                val wheelX = x + turnAmount * normalPosition.y
+                val wheelY = y - turnAmount * normalPosition.x
                 val wheelSpeed = hypot(wheelX, wheelY) * speed
                 val wheelAngle = atan2(wheelY, wheelX)
                 map[positionModule] = wheelSpeed
